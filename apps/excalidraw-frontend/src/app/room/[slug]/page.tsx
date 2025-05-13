085d90518130197f7deb5ed48d7fe39808c03f97
yash sharma <yashsharma0632@gmail.com>
2025-05-13 20:35:00 +0530

    created_rectangle

--- apps/excalidraw-frontend/src/app/room/[slug]/page.tsx
+++ apps/excalidraw-frontend/src/app/room/[slug]/page.tsx
@@ -1,7 +1,61 @@
+"use client"
+
+import { useEffect, useRef } from 'react';
+
 export default function RoomPage(){
+   
+
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+    
+    
+    useEffect(()=>{
+        if(canvasRef.current){
+            
+            const canvas = canvasRef.current;
+            const ctx = canvas.getContext('2d');
+
+            if(!ctx){
+                return;
+            }
+            let clicked = false;
+            let startX = 0;
+            let startY = 0;
+
+            
+            canvas.addEventListener("mousedown",(e)=>{
+                clicked = true;
+                startX = e.clientX;
+                startY = e.clientY;
+            })
+
+            canvas.addEventListener("mouseup",(e)=>{
+                clicked = false;
+                
+            })
+            
+            canvas.addEventListener("mousemove",(e)=>{
+                if(clicked){
+                    const width = e.clientX - startX;
+                    const heigth = e.clientY - startY;
+                    console.log(width,heigth);
+                    ctx.clearRect(0,0,canvas.width,canvas.height);
+                    ctx.strokeRect(startX,startY,width,heigth);
+                }
+                
+            })
+            
+        }
+    },[])
+    
+
+    
+
     return(
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            Room Page
-        </div>
+        <div className="w-screen h-screen flex justify-center items-center bg-white">
+            <canvas ref={canvasRef} width={500} height={500}></canvas>
+        </div> 
     )
+
+
+   
 }
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -5,14 +5,20 @@ import { ReactNode } from "react";
 interface ButtonProps {
   children: ReactNode;
   className?: string;
-  appName: string;
+  variant?: "primary" | "outlined" | "secondary"
+  size?: "lg" | "md",
+  onClick?:()=>void
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children, className, variant,size,onClick }: ButtonProps) => {
   return (
     <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      className={`${className} 
+        ${variant == "primary" ? "bg-primary": variant == "secondary"? "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80":"border border-input bg-background hover:bg-accent hover:text-accent-foreground shadow-sm"} 
+        ${size === "lg" ? "px-4 py-2":"px-2 py-1"}`
+      }
+      
+      onClick={onClick}
     >
       {children}
     </button>
